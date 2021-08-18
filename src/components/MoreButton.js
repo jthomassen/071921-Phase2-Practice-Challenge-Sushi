99d878fe9615ec0fbb548a284551515a8be94604
Jeff Thomassen <jeff.thomassen@ucdenver.edu>
2021-08-18 12:05:09 -0600

    complete

--- src/components/MoreButton.js
+++ src/components/MoreButton.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-function MoreButton(props) {
-  return <button onClick={/* Fill me in! */ null}>More sushi!</button>;
+function MoreButton({moreSushiHandler}) {
+  return <button onClick={moreSushiHandler}>More sushi!</button>;
 }
 
 export default MoreButton;
+
